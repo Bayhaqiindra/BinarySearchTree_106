be6e1855e8ee3f6748ee83c2c5fbb6bdeee51a23
bayhaqiindra <bayhaqi30072004@gmail.com>
2023-06-29 11:25:34 +0700

    untuk menyimpan simpul yang akan menjadi induk (parent) dari simpul yang akan dimasukkan.

--- BinarySearchtree_106/BinarySearchtree_106.cpp
+++ BinarySearchtree_106/BinarySearchtree_106.cpp
@@ -35,4 +35,5 @@ public:
 		newNode->leftchild = NULL; // Make the left child of the new node point to NULL
 		newNode->rightchild = NULL; // Make the right child of the new node point to NULL
 
+		Node* parent = NULL;
 		
